9d6b6ad8bde1eb1a0202f938ad1e4e41814d6620
Ronald Franco <4780483+Francoded@users.noreply.github.com>
2024-05-01 10:20:33 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import logo from "./images/logo.png";
 export default function Board() {
   return (
     <>
-    <div class="row">
-      <div class="col-4 col-sm-3"></div>
-      <img src={logo} class="logo col-4 col-sm-6" alt="Logo" />;
-      <div class="col-4 col-sm-3"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-4 col-sm-3"></div>
+        <img src={logo} class="logo col-4 col-sm-6" alt="Logo" />;
+        <div class="col-4 col-sm-3"></div>
+      </div>
     </div>
     </>
   );
